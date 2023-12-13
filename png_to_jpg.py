224c2891758dd282812349fc5008b9041868cd68
Andriyanto Halim <andriyanto.halim@gmail.com>
2023-12-13 20:54:34 +0800

    modify png_to_jpg.py implementation to take in both folder and single file. implement get_exif()

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from PIL import Image
 
 
@@ -13,23 +14,36 @@ def get_png_files(file_path):
     return _png_files
 
 
-def convert_to_png(input_file):
-    png_img = Image.open(input_file)
-    name, ext = os.path.splitext(input_file)
+def convert_to_png(file_path):
+    if not os.path.isfile(file_path):
+        print("Input is not a file.")
+        return
+
+    png_img = Image.open(file_path)
+    name, ext = os.path.splitext(file_path)
+
+    if ext != ".png":
+        print("Not a PNG file.")
+        return
+
+    if png_img.mode in ("RGBA", "P"):
+        png_img = png_img.convert("RGB")
+
     png_img.save(name + str(".jpg"))
 
 
 def main():
-    path = "."
-
-    png_files = get_png_files(path)
-
-    if not png_files:
-        print("No PNG files detected.")
+    if len(sys.argv) == 1:
+        print("Please enter file path and file name")
         return
 
-    for png in png_files:
-        convert_to_png(png)
+    if os.path.isfile(sys.argv[1]):
+        convert_to_png(sys.argv[1])
+    else:
+        png_files = get_png_files(sys.argv[1])
+
+        for file in png_files:
+            convert_to_png(sys.argv[1] + "\\" + file)
 
 
 if __name__ == "__main__":
